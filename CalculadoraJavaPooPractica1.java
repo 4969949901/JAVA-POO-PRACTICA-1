095f568598addddda73157122608e792dd8dab11
Helen <lissa.acevedo99@gmail.com>
2022-03-19 10:11:20 -0600

    Suma

---
+++ CalculadoraJavaPooPractica1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Calculadora ;
+
+/**
+ *
+ * @author helac
+ */
+ public class CalculadoraJavaPooPractica1 {
+    private int pantalla;
+    private int teclas;
+    private String calculadora;
+
+public CalculadoraJavaPooPractica1(){
+       this.calculadora="Calculadora con 6 funciones basicas";
+       this.teclas=20;
+       this.pantalla=15;
+       
+   }
+   public double sumar (double nouno,double nodos){
+   return nouno+nodos;
+   }
+
+}
+
+    
+    
+ 
